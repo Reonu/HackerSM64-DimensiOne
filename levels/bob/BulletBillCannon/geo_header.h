bc6834f837974738a9ef9c6c0dffeb1755cc5a50
Reonu <danileon95@gmail.com>
2022-11-19 18:22:20 +0100

    challenge 15

---
+++ levels/bob/BulletBillCannon/geo_header.h
@@ -0,0 +1,16 @@
+extern const GeoLayout BulletBillCannon_geo[];
+extern Vtx BulletBillCannon_BulletBillCannon_mesh_layer_1_vtx_cull[8];
+extern Vtx BulletBillCannon_BulletBillCannon_mesh_layer_1_vtx_0[28];
+extern Gfx BulletBillCannon_BulletBillCannon_mesh_layer_1_tri_0[];
+extern Vtx BulletBillCannon_BulletBillCannon_mesh_layer_1_vtx_1[8];
+extern Gfx BulletBillCannon_BulletBillCannon_mesh_layer_1_tri_1[];
+extern Vtx BulletBillCannon_BulletBillCannon_mesh_layer_1_vtx_2[4];
+extern Gfx BulletBillCannon_BulletBillCannon_mesh_layer_1_tri_2[];
+extern Gfx mat_BulletBillCannon_cozies_noise_thing_yellow_layer1[];
+extern Gfx mat_revert_BulletBillCannon_cozies_noise_thing_yellow_layer1[];
+extern Gfx mat_BulletBillCannon_f3dlite_material_004_layer1[];
+extern Gfx mat_revert_BulletBillCannon_f3dlite_material_004_layer1[];
+extern Gfx mat_BulletBillCannon_quicksand_layer1[];
+extern Gfx mat_revert_BulletBillCannon_quicksand_layer1[];
+extern Gfx BulletBillCannon_BulletBillCannon_mesh_layer_1[];
+extern Gfx BulletBillCannon_material_revert_render_settings[];
