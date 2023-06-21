bb963a236ab4e0a837506dede78b3953dda10f53
thecozies <79979276+thecozies@users.noreply.github.com>
2023-06-21 12:26:22 -0500

    check if grabbable floor is actually slippery

--- src/game/mario_step.c
+++ src/game/mario_step.c
@@ -467,7 +467,7 @@ struct Surface *check_ledge_grab(struct MarioState *m, struct Surface *prevWall,
         || (*ledgeFloor) == NULL
         || ledgePos[1] < nextPos[1] + 100.0f
 #ifdef DONT_LEDGE_GRAB_STEEP_SLOPES
-        || (*ledgeFloor)->normal.y < COS25 // H64 TODO: check if floor is actually slippery
+        || floor_is_slippery(ledgeFloor, m)
         || (SURFACE_IS_QUICKSAND((*ledgeFloor)->type) && gChallengeLevel == 14)
 #endif
     ) {
